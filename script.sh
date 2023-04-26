b846aa37ca89fceb57c5f9560aacd04cd47fc5ba
TanKaiYi0220 <xiaopihaikaiyi@gmail.com>
2023-04-27 00:10:23 +0800

    feat: firewall rules add rule to INPUT add rule to Packet Forwarding add rule to Port Forwarding

--- script.sh
+++ script.sh
@@ -4,8 +4,8 @@ ip link add nsA-eth0 type veth peer name host-nsA
 ip link add nsB-eth0 type veth peer name host-nsB
 ip link set nsA-eth0 netns nsA
 ip link set nsB-eth0 netns nsB
-ip netns exec nsA ip addr add 10.233.55.2/24 dev nsA-eth0 # dev: device
-ip netns exec nsB ip addr add 10.233.55.3/24 dev nsB-eth0
+ip netns exec nsA ip addr add 10.33.55.2/24 dev nsA-eth0 # dev: device
+ip netns exec nsB ip addr add 10.33.55.3/24 dev nsB-eth0
 ip netns exec nsA ip link set nsA-eth0 up
 ip netns exec nsA ip link set lo up # lo: loopback device
 ip netns exec nsB ip link set nsB-eth0 up
@@ -13,13 +13,18 @@ ip netns exec nsB ip link set lo up
 ip link add name br0 type bridge
 ip link set host-nsA master br0
 ip link set host-nsB master br0
-ip addr add 10.233.55.1/24 brd + dev br0 # brd: broadcast
+ip addr add 10.33.55.1/24 brd + dev br0 # brd: broadcast
 ip link set host-nsA up
 ip link set host-nsB up
 ip link set br0 up
-ip netns exec nsA ip route add default via 10.233.55.1
-ip netns exec nsB ip route add default via 10.233.55.1
+ip netns exec nsA ip route add default via 10.33.55.1
+ip netns exec nsB ip route add default via 10.33.55.1
 echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -A FORWARD -o ens33 -i br0 -j ACCEPT # ens33 depend on Internet-connected device
 iptables -A FORWARD -i ens33 -o br0 -j ACCEPT
-iptables -t nat -A POSTROUTING -s 10.233.55.1/24 -o ens33 -j MASQUERADE # IP Masquerading
+iptables -t nat -A POSTROUTING -s 10.33.55.1/24 -o ens33 -j MASQUERADE # IP Masquerading
+iptables -A INPUT -p tcp -m tcp --dport 22 -s 103.136.224.0/29 -j ACCEPT
+iptables -A INPUT -p tcp -m tcp --dport 22 -s 103.136.224.8/29 -j REJECT
+iptables -A FORWARD -d 103.136.225.150 -o br0 -j REJECT
+iptables -A PREROUTING -t nat -p tcp --dport 7777 -j DNAT --to-destination 10.33.55.2:8888
+iptables -A FORWARD -p tcp -d 10.33.55.2 --dport 8888 -j ACCEPT
