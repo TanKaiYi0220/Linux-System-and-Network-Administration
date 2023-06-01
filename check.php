7aa2fe77f12516bc342049b2ba68d318e85dfdcd
TanKaiYi0220 <xiaopihaikaiyi@gmail.com>
2023-06-01 11:52:34 +0800

    feat: load balancer docker mysql wordpress haproxy

---
+++ check.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>PHP Testing</title>
+    </head>
+    <body>
+        <?php echo '<p>Hello World</p>'; ?>
+        <?php 
+            echo 'Server IP Address: '.'<fontcolor=red>'.$_SERVER['SERVER_ADDR'].'</font>'.'<br>';
+            echo 'Server Domain Name: '.'<fontcolor=red>'.$_SERVER['SERVER_NAME'].'</font>'.'<br>';
+        ?>
+    </body>
+</html>
